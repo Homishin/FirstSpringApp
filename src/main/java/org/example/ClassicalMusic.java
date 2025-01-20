1d8cb76084bd5a6b2f707d7973b99fb532ce4fba
Homishin <homieshin@gmail.com>
2025-01-20 22:52:18 +0500

    Bean Lifecycle Scope, init, destroy, Factory

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic(){}
 
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+
+    public void doMyInit(){
+        System.out.println("ClassicalMusic doMyInit");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("ClassicalMusic doMyDestroy");
+    }
 
     @Override
     public String getSong() {
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,11 +6,18 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        ClassicalMusic classicalMusic = (ClassicalMusic) context.getBean("classicalMusic");
+        ClassicalMusic classicalMusic2 = (ClassicalMusic) context.getBean("classicalMusic");
+
+        boolean jaja = classicalMusic == classicalMusic2;
+
+        System.out.println(jaja);
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        //MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.play();
+        //musicPlayer.play();
         context.close();
     }
 }
