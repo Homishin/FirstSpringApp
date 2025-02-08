ec9d755ecbc8ad12c6cd44cdb24f5bb7a61d2ef1
Homishin <homieshin@gmail.com>
2025-02-08 23:43:30 +0500

    @configuration

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -2,12 +2,18 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
     private Music music1;
     private Music music2;
     private TypeOfMusic typeOfMusic;
@@ -17,6 +23,21 @@ public class MusicPlayer {
         this.music2 = music2;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public String play(TypeOfMusic typeOfMusic) {
         if (typeOfMusic == TypeOfMusic.CLASSICALMUSIC){
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,14 +1,25 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
-        context.close();
+//        Computer computer = context.getBean("computer", Computer.class);
+//        System.out.println(computer);
+//        context.close();
+
+        MusicPlayer musicPlayer = (MusicPlayer) context.getBean("musicPlayer");
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = (ClassicalMusic) context.getBean("classicalMusic");
+        ClassicalMusic classicalMusic2 = (ClassicalMusic) context.getBean("classicalMusic");
+
+        System.out.println(classicalMusic1 == classicalMusic2);
 
     }
 }
